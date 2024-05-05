a58f092b12f6c5cf37742d0e342b61c27b76bcf8
Cody <cody.wildtyto@gmail.com>
2024-05-05 00:37:54 -0400

    feat(general): add basic content

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,136 @@
 function App() {
     return (
-        <div className="container">
-            <h1 className="text-3xl font-bold">Awesork</h1>
-        </div>
+        <>
+            <section className="bg-sky-500 text-white">
+                <div className="container max-w-none w-5/6">
+                    <div>Awesork Present</div>
+                    <p>noper</p>
+                    <p>工作，不為堆積的事焦額。</p>
+                    <p>LINE討論工作還能隨記待辦事項</p>
+                </div>
+            </section>
+            <section className="">
+                <div className="container">
+                    <h1 className="">更聰明的方法，做事更快速。</h1>
+                    <div className="grid grid-cols-5 py-16 text-center">
+                        <span>事項記錄</span>
+                        <span>對話新增</span>
+                        <span>任務調派</span>
+                        <span>待辦提醒</span>
+                        <span>輕易使用</span>
+                    </div>
+                </div>
+            </section>
+            <section className="bg-gray-100">
+                <div className="container">
+                    <div className="w-1/3">
+                        <h2 className="">事項記錄</h2>
+                        <p>
+                            隨時透過 noper
+                            寫下任何事項，作為任務安排、或者團隊筆記，除了有足夠空間備註事情的描述外，可以為每一則事項設置參與者、分類、目標日期、以及執行狀態，方便工作夥伴和自己能輕鬆又快速查閱與管理。
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <section className="">
+                <div className="container flex flex-row-reverse">
+                    <div className="w-1/3">
+                        <h2 className="">對話新增</h2>
+                        <p>
+                            現在只要在 LINE
+                            群組輸入『noper』或縮寫『np』加上事項標題，就可以瞬間新增好一件工作待辦事項，用
+                            @ 標註群組成員還能同時指派並通知給他們。
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <section className="bg-gray-100">
+                <div className="container">
+                    <div className="w-1/3">
+                        <h2 className="">任務調派</h2>
+                        <p>
+                            被指派事項的參與者，可以即時收到 noper
+                            的通知，讓任務的派遣能無縫傳遞，事項建立者也能在事項執行狀態被標註完成時收到通知，再配合分類的設置，加強流程及組織上的清晰。
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <section className="">
+                <div className="container flex flex-row-reverse">
+                    <div className="w-1/3">
+                        <h2 className="">待辦提醒</h2>
+                        <p>
+                            不再擔心忙碌中忽略的通知，只要設定提醒時間，每天
+                            noper 都會定時透過通訊軟體 LINE
+                            報告待辦的執行程度，配合目標日期及參與者的設置，更能一目瞭然當日每個工作夥伴的事項進度。
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <section className="bg-green-300">
+                <div className="container">
+                    <div className="w-1/3 text-white">
+                        <h2 className="">輕易使用</h2>
+                        <p>
+                            好用的工具加上便利的情境，不僅可以在瀏覽器上使用，甚至在通訊軟體
+                            LINE 中還有量身定做的操作模式，只要將 noper 加入
+                            LINE
+                            群組馬上就享有所有最有效率的功能，完全免申請帳號、免安裝軟體。
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <section className="">
+                <div className="container">
+                    <h1 className="">一覽實用功能。</h1>
+                    <div className="grid grid-cols-3 gap-12 py-16 text-center">
+                        <span>
+                            事項。簡單又靈活地開啟任務以及筆記，工作夥伴能夠顯而易見了解工作狀況。
+                        </span>
+                        <span>
+                            參與者。事項負責人能即時透過多項功能獲取告知，讓任務傳遞無縫接軌。
+                        </span>
+                        <span>
+                            分類。將大量事項清楚分割不混淆在一起，幫助工作夥伴快速查閱內容。
+                        </span>
+                        <span>
+                            目標日期。為事項加上時間性，經由提醒功能幫助團隊確實掌握每天任務。
+                        </span>
+                        <span>輕易使用</span>
+                    </div>
+                </div>
+            </section>
+            <section className="bg-sky-300">
+                <div className="container">
+                    <h1 className="text-white">選擇適合的方案。</h1>
+                    <div className="grid grid-cols-5 gap-12 py-16 text-center">
+                        <span>免費</span>
+                        <span>一般團隊</span>
+                        <span>大型組織</span>
+                        <span>專案經理人</span>
+                        <span>企業管理者</span>
+                    </div>
+                </div>
+            </section>
+            <section className="">
+                <div className="container">
+                    <h1 className="text-white">馬上使用。</h1>
+                    <div className="grid grid-cols-4 gap-12 py-16 text-center">
+                        <span>掃描 QR Code 條碼或者點此開啟</span>
+                        <span>顯現佈告板 noper 的好友畫面點擊「加入」</span>
+                        <span>
+                            進到欲使用佈告板 noper 的群組從副選單「邀請」
+                        </span>
+                        <span>從好友列表選擇佈告板 noper邀進群組即可開始</span>
+                    </div>
+                </div>
+            </section>
+            <section className="h-36">
+                <div className="container text-center">
+                    <p>Copyright © 2019 Awesork co., Ltd.</p>
+                </div>
+            </section>
+        </>
     );
 }
 
